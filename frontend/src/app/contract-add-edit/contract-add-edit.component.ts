485c9b9afcf1bcad73a46d7f6e174da25e6a1950
IT21302862 <sanayasamadhi@gmail.com>
2023-09-19 14:26:08 +0530

    seson & supplement add contract form

--- frontend/src/app/contract-add-edit/contract-add-edit.component.ts
+++ frontend/src/app/contract-add-edit/contract-add-edit.component.ts
@@ -1,59 +1,52 @@
-import { Component,Inject,OnInit } from '@angular/core';
-import { HotelContractDTO } from '../services/HotelContractDTO ';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HotelContractDTO } from '../services/HotelContractDTO ';// Correct the import path
 import { HotelService } from '../services/hotels.service';
-import { FormBuilder, FormGroup ,Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, AbstractControl } from '@angular/forms'; // Import AbstractControl
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CoreService } from '../core/core.service';
-import { FormArray } from '@angular/forms';
+import { SeasonDTO } from '../services/SeasonDTO'; // Import the Season interface
 
 @Component({
   selector: 'app-contract-add-edit',
   templateUrl: './contract-add-edit.component.html',
   styleUrls: ['./contract-add-edit.component.css']
 })
-export class ContractAddEditComponent implements OnInit{
+export class ContractAddEditComponent implements OnInit {
+  contractFormGroup!: FormGroup;
+  seasons!: FormArray; // Define seasons as a FormArray
+  supplements!:FormArray;
 
-  contractForm: FormGroup;
-
-  constructor(private _fb:FormBuilder,
-    private _hotelService:HotelService,
-    private _dialogRef:MatDialogRef<ContractAddEditComponent>,
-    @Inject(MAT_DIALOG_DATA) public data:HotelContractDTO,
-    private _coreService:CoreService
-      
-    ) {
-     this.contractForm= this._fb.group({
-      contractID: [0],
-      startDate: [new Date(), Validators.required],
-      endDate: [new Date(), Validators.required],
-      termsAndConditions: ['Sample terms and conditions', Validators.required],
-      hotel: this._fb.group({
-        hotelID: [0]
-      }),
-      seasons: this._fb.array([this.createSeason()]),
-      supplements: [[]],
-      supplementPrices: [[]],
-      markupPrices: [[]],
-      discounts: this._fb.group({
-        discountPercentage: [0.1, [Validators.required, Validators.min(0)]],
-        noOfDates: [0, [Validators.required, Validators.min(0)]]
-      }),
-      roomTypes: [[]],
-      roomTypePrices: [[]]
-    });
-
-
-  }
+  constructor(
+    private _fb: FormBuilder,
+    private _hotelService: HotelService,
+    private _dialogRef: MatDialogRef<ContractAddEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: HotelContractDTO,
+    private _coreService: CoreService
+  ) {}
 
   ngOnInit(): void {
-    this.contractForm.patchValue(this.data);
+    this.contractFormGroup = this.createContractForm();
+    this.seasons = this.contractFormGroup.get('seasons') as FormArray; // Initialize seasons as a FormArray
+    this.supplements = this.contractFormGroup.get('supplements') as FormArray;
   }
 
   addSeason() {
-    const seasons = this.contractForm.get('seasons') as FormArray;
-    seasons.push(this.createSeason());
+    this.seasons.push(this.createSeason());
+  }
+
+  addSupplement() {
+    const supplement = this.createSupplements();
+    this.supplements.push(supplement);
   }
   
+
+  removeSeason(index: number) {
+    this.seasons.removeAt(index);
+  }
+
+  removeSupplement(index: number) {
+    this.supplements.removeAt(index);
+  }
 
   createSeason(): FormGroup {
     return this._fb.group({
@@ -63,32 +56,49 @@ export class ContractAddEditComponent implements OnInit{
     });
   }
 
+  createSupplements(): FormGroup {
+    return this._fb.group({
+      supplementName: [''],
+    });
+  }
+
+  createContractForm(): FormGroup {
+    return this._fb.group({
+      contractID: [0],
+      startDate: [new Date(), Validators.required],
+      endDate: [new Date(), Validators.required],
+      termsAndConditions: ['Sample terms and conditions', Validators.required],
+      hotel: this._fb.group({
+        hotelID: [0]
+      }),
+      seasons: this._fb.array([]),
+      supplements: this._fb.array([]),
+      supplementPrices: [[]],
+      markupPrices: [[]],
+      discounts: this._fb.group({
+        discountPercentage: [0.1, [Validators.required, Validators.min(0)]],
+        noOfDates: [0, [Validators.required, Validators.min(0)]]
+      }),
+      roomTypes: [[]],
+      roomTypePrices: [[]]
+    });
+  }
 
   onSubmit() {
-    if (this.contractForm.valid) {
-      const formData = this.contractForm.value as HotelContractDTO;
-  
-      // Assuming you have a method in your hotel service to create a contract
-      // You can replace 'createHotelContract' with the actual method name in your service
+    if (this.contractFormGroup.valid) {
+      const formData = this.contractFormGroup.value as HotelContractDTO;
+
       this._hotelService.createHotelContract(formData).subscribe(
         (response) => {
-          // Handle success, e.g., show a success message, close the dialog, etc.
           console.log('Contract created successfully:', response);
           this._dialogRef.close();
         },
         (error) => {
-          // Handle error, e.g., display an error message
           console.error('Error creating contract:', error);
         }
       );
     } else {
-      // Form is invalid, mark all fields as touched to display validation errors
-      this.contractForm.markAllAsTouched();
+      this.contractFormGroup.markAllAsTouched();
     }
   }
-  
-  
-
-
-
 }
